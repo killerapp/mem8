c871432d25cbc1f9e362d99be977c27b8ff71d19
killerapp <vaskin@gmail.com>
2025-10-07 09:30:14 -0500

    chore: configure Cloudflare deployment to codebasecontext.org
    - Update Cloudflare Pages project name to codebase-context-site
    - Change production URL from docs.codebasecontext.org to codebasecontext.org
    - Configure wrangler deployment in GitHub Actions workflow

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.codebasecontext.org',
+  url: 'https://codebasecontext.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/',
 
